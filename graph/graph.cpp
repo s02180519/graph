bd681934214c0c156a5e899128a9b0554649b732
Olya <belyaevaolga881@gmail.com>
2020-11-26 00:34:41 +0300

    smth

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,12 +1,19 @@
 ﻿// graph.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
+//#include <iostream>
+#define GLEW_STATIC
+#include<GL/glew.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+#include<GL/freeglut.h>
+#include<glm.hpp>
+#include<gtx/transform.hpp>
+
+
+//int main()
+//{
+//    std::cout << "Hello World!\n";
+//}
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
@@ -18,3 +25,103 @@ int main()
 //   4. В окне "Список ошибок" можно просматривать ошибки.
 //   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
 //   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+
+#include <iostream>
+
+//#include <freeglut.h>
+
+using namespace std;
+
+float WinWid = 400, WinHei = 400;
+
+float angle = 0.0, scale = 1.0;
+
+void timer(int value) {
+
+	glColor3f(0.5, 1.0, 1.0);
+
+	angle++;
+
+	glRotatef(15.0, 10.0, 20.0, 1.0);
+
+	glutPostRedisplay();
+
+	glutTimerFunc(70, timer, 0);
+
+}
+
+void init() {
+
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	glMatrixMode(GL_PROJECTION);
+
+	glLoadIdentity();
+
+	glOrtho(-WinWid / 2, WinWid / 2, -WinHei / 2, WinHei / 2, -200.0, 200.0);
+
+	glMatrixMode(GL_MODELVIEW);
+
+}
+
+void draw() {
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glPushMatrix();
+
+	scale = 1.0 - abs(sin(3.14 * angle / 90.0) / sqrt(3.0) / 2);
+
+	glRotatef(angle, 0.0, 0.0, 1.0);
+
+	glScalef(scale, scale, 1.0);
+
+	glBegin(GL_LINES);
+
+	for (float i = -WinWid / 2; i <= WinWid / 2; i += 20.0) {
+
+		glVertex2f(i, -WinHei / 2);
+
+		glVertex2f(i, WinHei / 2);
+
+	}
+
+	for (float i = -WinWid / 2; i <= WinWid / 2; i += 20.0) {
+
+		glVertex2f(-WinWid / 2, i);
+
+		glVertex2f(WinWid / 2, i);
+
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	glutSwapBuffers();
+
+}
+
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+	glutInitWindowSize(WinWid, WinHei);
+
+	glutInitWindowPosition(400, 150);
+
+	glutCreateWindow("WebWindow");
+
+	glutDisplayFunc(draw);
+
+	glutTimerFunc(1000, timer, 0);
+
+	init();
+
+	glutMainLoop();
+}
+
+
