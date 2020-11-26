962c0224e6dc95d53e6c913a77ae4267fdaa21a0
Olya <belyaevaolga881@gmail.com>
2020-11-26 03:48:47 +0300

    shaders for triangle

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -39,13 +39,91 @@ int main()
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
-    while (!glfwWindowShouldClose(window))     // play cycle
+
+    /************* TRIANGLE ***************/
+    // vertex data
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // vertex shader
+    const char* vertexShaderStr =
+        "#version 330 core\n"
+        "layout(location = 0) in vec3 position;\n"
+        "void main(){\n"
+        "   gl_Position = vec4(position.x, position.y, position.z, 1.0);\n }";
+
+    // build shader
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderStr, NULL);
+    glCompileShader(vertexShader);
+
+    // errors
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // fragment shader
+    const char* fragmentShaderStr =
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "void main(){ \n"
+        "color = vec4(1.0f, 0.5f, 0.2f, 1.0f); \n}";
+
+    // build
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderStr, NULL);
+    glCompileShader(fragmentShader);
+
+    // shader program
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    //errors
+    glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // we want use program
+    glUseProgram(shaderProgram);
+
+    // delete shaders
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    /****************** PLAY CYCLE ********************/
+
+    while (!glfwWindowShouldClose(window))     
     {
         // check events
         glfwPollEvents();
 
+        glClearColor(0.1f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         // drow here
 
+        
         // we have 2 buffers (front back). we see front buffer when back buffer has been drowing
         // then swap buffers
         glfwSwapBuffers(window);
