92dc0449a4c715b00487ed29c47f4b7269a24a38
Olya <belyaevaolga881@gmail.com>
2020-12-01 04:16:54 +0300

    2 figure

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -64,6 +64,41 @@ int main()
 
     Shader Triangle = Shader("shaders/triangle.vs.txt", "shaders/triangle.fs.txt");
 
+    /***************** SQUARE *********************/
+
+    GLfloat square_vert[] = {
+     0.5f,  0.5f, 0.0f,  // Верхний правый угол
+     0.5f, -0.5f, 0.0f,  // Нижний правый угол
+    -0.5f, -0.5f, 0.0f,  // Нижний левый угол
+    -0.5f,  0.5f, 0.0f   // Верхний левый угол
+    };
+
+    GLuint indices[] = {  
+    0, 1, 3,   // Первый треугольник
+    1, 2, 3    // Второй треугольник
+    };
+
+    GLuint EBO, VAO_square, VBO_square;
+    glGenVertexArrays(1, &VAO_square);
+    glGenBuffers(1, &VBO_square);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO_square);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_square);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(square_vert), square_vert, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+    glBindVertexArray(0);
+
+    Shader Square = Shader("shaders/square.vs.txt", "shaders/triangle.fs.txt");
+
     /****************** PLAY CYCLE ********************/
 
     while (!glfwWindowShouldClose(window))     
@@ -79,6 +114,12 @@ int main()
         Triangle.Use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        Square.Use();
+        glBindVertexArray(VAO_square);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         
