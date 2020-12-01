a25cdf0655a9df2c90182fd37cdc74d57894e38b
Olya <belyaevaolga881@gmail.com>
2020-12-01 03:29:01 +0300

    its work

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -62,63 +62,7 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-    // vertex shader
-    const char* vertexShaderStr =
-        "#version 330 core\n"
-        "layout(location = 0) in vec3 position;\n"
-        "void main(){\n"
-        "   gl_Position = vec4(position.x, position.y, position.z, 1.0);\n }";
-
-    // build shader
-    GLuint vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderStr, NULL);
-    glCompileShader(vertexShader);
-
-    // errors
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // fragment shader
-    const char* fragmentShaderStr =
-        "#version 330 core\n"
-        "out vec4 color;\n"
-        "void main(){ \n"
-        "color = vec4(1.0f, 0.0f, 0.5f, 1.0f); \n}";
-
-    // build
-    GLuint fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderStr, NULL);
-    glCompileShader(fragmentShader);
-
-    // shader program
-    GLuint shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    //errors
-    glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
-    // we want use program
-    glUseProgram(shaderProgram);
-
-    // delete shaders
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Shader Triangle = Shader("shaders/triangle.vs.txt", "shaders/triangle.fs.txt");
 
     /****************** PLAY CYCLE ********************/
 
@@ -131,7 +75,8 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // drow here
-        glUseProgram(shaderProgram);
+        //glUseProgram(shaderProgram);
+        Triangle.Use();
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
