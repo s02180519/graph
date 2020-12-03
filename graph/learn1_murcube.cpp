549938f9747d39caccf9b161e4d096f717ea87ef
Olya <belyaevaolga881@gmail.com>
2020-12-03 21:19:09 +0300

    1 coal cube

--- graph/learn1_murcube.cpp
+++ graph/learn1_murcube.cpp
@@ -1,4 +1,5 @@
-п»ї#define GLEW_STATIC
+/*
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -31,7 +32,7 @@ bool firstMouse = false;
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
-/************* TRIANGLE ***************/
+/************* TRIANGLE ***************
     // vertex data
 GLfloat vertices[] = {
     -0.5f, -0.5f, 0.0f,
@@ -40,16 +41,16 @@ GLfloat vertices[] = {
 };
 
 GLfloat square_vert[] = {
-    // РџРѕР·РёС†РёРё            // Р¦РІРµС‚Р°            // РўРµРєСЃС‚СѓСЂРЅС‹Рµ РєРѕРѕСЂРґРёРЅР°С‚С‹
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Р’РµСЂС…РЅРёР№ РїСЂР°РІС‹Р№
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // РќРёР¶РЅРёР№ РїСЂР°РІС‹Р№
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // РќРёР¶РЅРёР№ Р»РµРІС‹Р№
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Р’РµСЂС…РЅРёР№ Р»РµРІС‹Р№
+    // Позиции            // Цвета            // Текстурные координаты
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Верхний правый
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Нижний правый
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Нижний левый
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Верхний левый
 };
 
 GLuint indices[] = {
-    0, 1, 3,   // РџРµСЂРІС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє
-    1, 2, 3    // Р’С‚РѕСЂРѕР№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє
+    0, 1, 3,   // Первый треугольник
+    1, 2, 3    // Второй треугольник
 };
 
 GLfloat cube_vert[] = {
@@ -153,7 +154,7 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-    
+
     glfwSetKeyCallback(window, key_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
@@ -167,13 +168,13 @@ int main()
         std::cout << "Failed to initialize GLEW" << std::endl;
         return -1;
     }
-    
+
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     //glfwSetKeyCallback(window, key_callback);
 
 
-    /***********************************************/
+    /***********************************************
     glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
     std::cout << vec.x << vec.y << vec.z << std::endl;
     glm::mat4 trans = glm::mat4(1.0f);
@@ -181,9 +182,26 @@ int main()
     vec = trans * vec;
     std::cout << vec.x << vec.y << vec.z << std::endl;
 
-    /***************** SQUARE *********************/
+    /*
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    GLuint textureCoal = loadTexture("textures/coal.jpg");
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    Shader Triangle = Shader("shaders/triangle.vs", "shaders/triangle.fs");
+    */
+
+
+    /***************** SQUARE *********************
+
+    GLuint textureWow = loadTexture("textures/wow.jpg");
 
     GLuint EBO, VAO_square, VBO_square;
     glGenVertexArrays(1, &VAO_square);
@@ -201,7 +219,7 @@ int main()
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
     // Color attribute
-    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+   // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
     //glEnableVertexAttribArray(1);
     // TexCoord attribute
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
@@ -214,9 +232,9 @@ int main()
     Shader Square = Shader("shaders/square.vs", "shaders/square.fs");
 
     glEnable(GL_DEPTH_TEST);
-    /****************** PLAY CYCLE ********************/
+    /****************** PLAY CYCLE ********************
 
-    while (!glfwWindowShouldClose(window))     
+    while (!glfwWindowShouldClose(window))
     {
         // check events
         glfwPollEvents();
@@ -233,25 +251,31 @@ int main()
         glClearColor(0.2f, 0.0f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //glActiveTexture(GL_TEXTURE0);
+        // drow here
+        //glUseProgram(shaderProgram);
+        //Triangle.Use();
+        //glBindVertexArray(VAO);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(0);
 
-        glBindTexture(GL_TEXTURE_2D, textureCoal);
-        glUniform1i(glGetUniformLocation(Square.Program, "ourTexture"), 0);
+        //glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textureWow);
+        //glUniform1i(glGetUniformLocation(Square.Program, "ourTexture"), 0);
 
         Square.Use();
-        //GLfloat timeValue = glfwGetTime()
 
-        /******************** TRANSFORM ************************/
+        /******************** TRANSFORM ************************
         glm::mat4 transform = glm::mat4(1.0f);
         //transform = glm::translate(transform, glm::vec3(1.0f, 1.0f, 0.0f));
         //transform = glm::rotate(transform, 90.0f, glm::vec3(0.0, 0.0, 1.0));
         //transform = glm::scale(transform, glm::vec3(0.5, 0.5, 0.5));
         //transform = glm::rotate(transform, (GLfloat)glfwGetTime() * 0.5f, glm::vec3(0.0f, 0.0f, 1.0f));
 
-        /******************** COORD **************************/
+        /******************** COORD **************************
 
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (GLfloat)glfwGetTime() * 0.5f, glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f));
 
         glm::mat4 view = glm::mat4(1.0f);
         //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
@@ -261,7 +285,7 @@ int main()
         float screenHeight = 600.0f;
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(camera.Zoom, (float)screenWidth / (float)screenHeight, 0.1f, 1000.0f);
-        
+
         GLint modelLoc = glGetUniformLocation(Square.Program, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
@@ -275,23 +299,17 @@ int main()
         GLint transformLoc = glGetUniformLocation(Square.Program, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
-
-        GLfloat greenValue = (sin(glfwGetTime()) / 2) + 0.5f;
-        GLfloat blueValue = (sin(glfwGetTime()) / 2) + 0.5f;
-        GLint vertexColorLocation = glGetUniformLocation(Square.Program, "ourColor");
-        glUniform3f(vertexColorLocation, 1.0f, greenValue, blueValue);
-
         glBindVertexArray(VAO_square);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
-        
+
         // we have 2 buffers (front back). we see front buffer when back buffer has been drowing
         // then swap buffers
         glfwSwapBuffers(window);
     }
-    
+
     glfwTerminate();
     return 0;
 }
@@ -347,3 +365,4 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
 }
+*/
